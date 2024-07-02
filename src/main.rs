5dd9c0352c6aecee9a7bec91daec31c62b8b4e61
Talat Fakhri <talatprog@gmail.com>
2024-07-03 00:57:11 +0800

    vanilla reg and login

--- src/main.rs
+++ src/main.rs
@@ -7,44 +7,21 @@ use tokio_tungstenite::WebSocketStream;
 use tokio_tungstenite::MaybeTlsStream;
 // use std::net::TcpStream;
 use tokio::net::TcpStream;
+use std::sync::{Arc, Mutex};
+use tokio::sync::mpsc;
 
 
-async fn receive_server_message(read: &mut futures_util::stream::SplitStream<WebSocketStream<TcpStream>>) -> Result<String, Box<dyn std::error::Error>> {
-    if let Some(message) = read.next().await {
-        match message? {
-            Message::Text(text) => Ok(text),
-            _ => Err("Received non-text message".into()),
-        }
-    } else {
-        Err("No message received".into())
-    }
-}
 
-async fn handle_login(write: &mut futures_util::stream::SplitSink<WebSocketStream<TcpStream>, Message>, 
-    read: &mut futures_util::stream::SplitStream<WebSocketStream<TcpStream>>) -> Result<(), Box<dyn std::error::Error>> {
-// Send login command
-    let login_command = "/login username password";
-    write.send(Message::Text(login_command.to_string())).await?;
-
-    // Receive the server's response
-    match receive_server_message(read).await {
-        Ok(response) => {
-            // Store the response in a variable
-            let login_result = response;
-
-            println!("Login result: {}", login_result);
-
-            if login_result == "ok" {
-                println!("Login successful");
-            } else {
-                println!("Login failed");
-            }
-        },
-        Err(e) => println!("Error receiving login result: {}", e),
-    }
-
-    Ok(())
-}
+// async fn receive_server_message(read: &mut futures_util::stream::SplitStream<WebSocketStream<TcpStream>>) -> Result<String, Box<dyn std::error::Error>> {
+//     if let Some(message) = read.next().await {
+//         match message? {
+//             Message::Text(text) => Ok(text),
+//             _ => Err("Received non-text message".into()),
+//         }
+//     } else {
+//         Err("No message received".into())
+//     }
+// }
 
 fn hash_message(message: &str) -> String {
     // Create a Sha256 hasher instance
@@ -60,30 +37,6 @@ fn hash_message(message: &str) -> String {
     hex::encode(result)
 }
 
-// async fn handle_login(stream: &mut WebSocketStream<MaybeTlsStream<TcpStream>>) -> Result<(), Box<dyn std::error::Error>> {
-//     let login_message = "/login username password".to_string(); // Replace with actual login details
-
-//     // Send the login message
-//     stream.send(Message::Text(login_message)).await?;
-
-//     // Wait for the server's response
-//     if let Some(message) = stream.next().await {
-//         match message? {
-//             Message::Text(text) => {
-//                 match text.as_str() {
-//                     "ok" => println!("Login successful"),
-//                     "notok" => println!("Login failed"),
-//                     _ => println!("Unexpected response: {}", text),
-//                 }
-//             },
-//             _ => println!("Received non-text message"),
-//         }
-//     } else {
-//         println!("No response received");
-//     }
-
-//     Ok(())
-// }
 
 fn print_instructions() -> io::Result<()> {
     println!("\x1b[94mEnter message (or '/quit' to exit): \x1b[0m");
@@ -108,44 +61,36 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("WebSocket handshake has been successfully completed");
 
     let (mut write, mut read) = ws_stream.split();
+    // let (tx, rx) = mpsc::channel::<Option<Vec<u8>>>(32);
+
 
     // Spawn a task to read messages from the server
-    // tokio::spawn(async move {
-    //     while let Some(message) = read.next().await {
-    //         match message {
-    //             Ok(msg) => println!("{}", msg),
-    //             Err(e) => eprintln!("Error receiving message: {}", e),
-    //         }
-    //     }
-    // });
+    tokio::spawn(async move {
+        while let Some(message) = read.next().await {
+            match message {
+                Ok(msg) => println!("Received: {}", msg),
+                Err(e) => eprintln!("Error receiving message: {}", e),
+            }
+        }
+    });
     print_instructions();
     let mut username= "";
     let mut token= "";
-    
+
 
 
     // Read input from the user and send it to the server
     loop {
+        
+        io::stdout().flush()?;
 
         let mut input = String::new();
         io::stdin().read_line(&mut input)?;
 
         let input = input.trim();
 
-        if input.eq_ignore_ascii_case("/quit") {
-            break;
-        }
-
-        if !(input.starts_with("/register") || input.starts_with("/login") || input.starts_with("/instructions")) {
-            println!("\x1b[33mYou need to be logged in to execute this instruction.\x1b[0m");
-            continue;
-        }
-
-        
         if input.starts_with("/register") || input.starts_with("/login") {
-            
             let mut words: Vec<&str> = input.split_whitespace().collect();
-            // println!("Words length: {}", words.len().to_string());
             if words.len() <= 2{
                 println!("\x1b[33mPass username and password! Please see instructions below.\x1b[0m");
                 print_instructions();
@@ -153,53 +98,28 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             } else {
                 let hash_pwd = hash_message(&words[2]);
                 words[2] = &hash_pwd;
-                let my_send_msg = words.join(" "); 
+                let my_send_msg = words.join(" ");
                 if input.starts_with("/login") {
                     println!("LOGIN");
-                    write.send(Message::Text(my_send_msg.to_string())).await?; 
-                    // >>>
-                    // Loop to read messages from the server
-                while let Some(message) = read.next().await {
-                    match message {
-                        Ok(msg) => {
-                            // Check if the message is a text message
-                            if msg.is_text() {
-                                // Get the text message
-                                let text = msg.into_text().expect("Failed to get text");
-
-                                // Print the received text message
-                                println!("Received:--> {}", text);
-                                break;
-                            }
-                        }
-                        Err(e) => {
-                            // Handle errors in receiving messages
-                            eprintln!("Error receiving message: {}", e);
-                        }
-                    }
-                }
-
-                    // <<<
-
-                } else {
-
                     write.send(Message::Text(my_send_msg.to_string())).await?;
-                }            
-
+                } else {
+                    write.send(Message::Text(my_send_msg.to_string())).await?;
+                }
             }
-            
-            
 
+        }
 
-
-        } else {
-            write.send(Message::Text(input.to_string())).await?;
+        if input.eq_ignore_ascii_case("/quit") {
+            break;
         }
         if input.eq_ignore_ascii_case("/instructions") {
             print_instructions();
         }
 
-
+        // write.send(Message::Text(input.to_string())).await?;
+        
+           
+        
     }
 
     println!("Disconnected");
---
+++ target/debug/deps/chat_app-6f740b9fa5c5a63d.d
@@ -0,0 +1,5 @@
+/Users/talat/backend/rust/hela_labs/chat_app_client/target/debug/deps/chat_app-6f740b9fa5c5a63d: src/main.rs
+
+/Users/talat/backend/rust/hela_labs/chat_app_client/target/debug/deps/chat_app-6f740b9fa5c5a63d.d: src/main.rs
+
+src/main.rs:
